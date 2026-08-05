8b3371fd12500e7763a492715c8153b3698dce9b
rennnn90ren-a11y <rennnn90ren@gmail.com>
2026-08-06 01:22:07 +0330

    Create admin.py

---
+++ handlers/admin.py
@@ -0,0 +1,26 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+
+from config import ADMIN_ID
+from keyboards.buttons import admin_keyboard
+
+
+router = Router()
+
+
+
+@router.message(
+    Command("panel")
+)
+async def panel(
+    message: types.Message
+):
+
+    if message.from_user.id != ADMIN_ID:
+        return
+
+
+    await message.answer(
+        "⚙️ پنل مدیریت",
+        reply_markup=admin_keyboard()
+)
