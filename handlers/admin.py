84999cc95e65cd92bd7d27269fa4122b9d693933
rennnn90ren-a11y <rennnn90ren@gmail.com>
2026-08-06 01:51:29 +0330

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,5 +1,5 @@
 from aiogram import Router, types
-from aiogram.filters import Command
+from aiogram.filters import CommandStart
 
 from config import ADMIN_ID
 from keyboards.buttons import admin_keyboard
@@ -9,18 +9,14 @@ router = Router()
 
 
 
-@router.message(
-    Command("panel")
-)
-async def panel(
+@router.message(CommandStart())
+async def admin_start(
     message: types.Message
 ):
 
-    if message.from_user.id != ADMIN_ID:
-        return
+    if message.from_user.id == ADMIN_ID:
 
-
-    await message.answer(
-        "⚙️ پنل مدیریت",
-        reply_markup=admin_keyboard()
-)
+        await message.answer(
+            "پنل مدیریت:",
+            reply_markup=admin_keyboard()
+        )
