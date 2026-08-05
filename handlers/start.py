e3dc1dfa17e327135bf83982a0af20f7791e2256
rennnn90ren-a11y <rennnn90ren@gmail.com>
2026-08-06 01:20:46 +0330

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,133 @@
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+
+
+from database import (
+    find_file,
+    get_channels
+)
+
+from keyboards.buttons import join_keyboard
+
+
+router = Router()
+
+
+
+async def is_member(bot, user_id):
+
+    channels = get_channels()
+
+
+    for channel in channels:
+
+        try:
+
+            member = await bot.get_chat_member(
+                channel,
+                user_id
+            )
+
+
+            if member.status in [
+                "left",
+                "kicked"
+            ]:
+                return False
+
+
+        except:
+
+            return False
+
+
+    return True
+
+
+
+@router.message(
+    CommandStart()
+)
+async def start_handler(
+    message: types.Message
+):
+
+    args = message.text.split()
+
+
+    if len(args) == 1:
+
+        await message.answer(
+            "سلام 👋\n"
+            "لینک چپتر را از کانال باز کنید."
+        )
+
+        return
+
+
+
+    code = args[1]
+
+
+    if not await is_member(
+        message.bot,
+        message.from_user.id
+    ):
+
+
+        await message.answer(
+            "برای دریافت فایل ابتدا عضو کانال‌ها شوید:",
+            reply_markup=join_keyboard(
+                get_channels()
+            )
+        )
+
+        return
+
+
+
+    file_id = find_file(
+        code
+    )
+
+
+    if file_id:
+
+
+        await message.answer_document(
+            document=file_id,
+            caption=f"📖 Chapter {code}"
+        )
+
+
+    else:
+
+        await message.answer(
+            "❌ چپتر پیدا نشد."
+        )
+
+
+
+@router.callback_query(
+    lambda c: c.data == "check_join"
+)
+async def check_join(
+    callback: types.CallbackQuery
+):
+
+    if await is_member(
+        callback.bot,
+        callback.from_user.id
+    ):
+
+        await callback.message.answer(
+            "✅ عضویت تایید شد.\n"
+            "حالا دوباره لینک چپتر را باز کن."
+        )
+
+    else:
+
+        await callback.answer(
+            "هنوز عضو همه کانال‌ها نشدی!",
+            show_alert=True
+)
