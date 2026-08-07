be11425cad00188370ce5de6eb9e78ed0fa94132
rennnn90ren-a11y <rennnn90ren@gmail.com>
2026-08-07 10:35:01 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-print("TOKEN:", BOT_TOKEN)
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN در متغیرهای محیطی تنظیم نشده!")
 
-ADMIN_ID = 8255361263
+ADMIN_ID = 8255361263  # ← اینو با آیدی واقعى خودت عوض کن
