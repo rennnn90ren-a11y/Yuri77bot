81c0d1d95b2ec2ba02778516c682c41cacdc72af
rennnn90ren-a11y <rennnn90ren@gmail.com>
2026-08-07 10:54:21 +0330

    Update config.pyظ

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_ID = 8255361263  # اول اینو تعریف کن
 
+# بعدش چاپ کن
 print("🔍 BOT_TOKEN موجود است:", "بله" if BOT_TOKEN else "خیر")
 print("🔍 ADMIN_ID:", ADMIN_ID)
 
 if not BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN در متغیرهای محیطی تنظیم نشده!")
-
-ADMIN_ID = 8255361263
