da2f3912c40121000ce7294aa4e845a58a305282
rennnn90ren-a11y <rennnn90ren@gmail.com>
2026-08-07 10:45:39 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+print("🔍 BOT_TOKEN موجود است:", "بله" if BOT_TOKEN else "خیر")
+print("🔍 ADMIN_ID:", ADMIN_ID)
+
 if not BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN در متغیرهای محیطی تنظیم نشده!")
 
-ADMIN_ID = 8255361263  # ← اینو با آیدی واقعى خودت عوض کن
+ADMIN_ID = 8255361263
